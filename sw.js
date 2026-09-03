37e4d1bad40a01b51312f35d26aeafe21e0a2d7c
PavelGH96 <pavel.demenshin@gmail.com>
2026-09-03 10:55:49 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* WiseWallet service worker: network-first для HTML (свежая версия сразу) + cache-first для ассетов + web push */
-const CACHE = "wisewallet-v4.03";
+const CACHE = "wisewallet-v4.04";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-192-maskable.png", "./icon-512-maskable.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
